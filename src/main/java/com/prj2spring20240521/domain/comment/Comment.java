1375fd981ef95566bcaefe8b28b5b73a5165a0bf
daeseong <zang5989@gmail.com>
2024-05-30 10:47:57 +0900

    20240530) 댓글 작성

---
+++ src/main/java/com/prj2spring20240521/domain/comment/Comment.java
@@ -0,0 +1,15 @@
+package com.prj2spring20240521.domain.comment;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Comment {
+    private Integer id;
+    // 하나의 게시물에 같은사람이 여러번 달 수 있어서 id가 필요, 다른것을 묶어서 PRIKEY 불가.
+    private Integer boardId;
+    private Integer memberId;
+    private String comment;
+    private LocalDateTime inserted;
+}
